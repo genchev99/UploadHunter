c31889b7693813180775fe9659fadb4898742ea4
genchev99 <genchev99@users.noreply.github.com>
2019-05-21 01:09:03 +0300

    Download button check

--- src/Index.js
+++ src/Index.js
@@ -2,6 +2,7 @@
 
 const puppeteer = require("puppeteer");
 const randomString = require("randomstring");
+const fs = require("fs");
 
 (async () => {
   try {
@@ -11,13 +12,37 @@ const randomString = require("randomstring");
     while (true) {
       const url = generateUrl();
       console.log(`[~] Opening: ${url}`);
-      await page.goto(url, {timeout: 200000, waitUntil: "networkidle2"})
+      await page.goto(url, {timeout: 200000, waitUntil: "networkidle2"});
+
+      if (await validateEndpoint(page)) {
+        /* In case the url contains download button/link */
+        fs.appendFileSync("scraped.txt", url);
+      }
     }
   } catch (e) {
     console.error(e);
     process.exit(1);
   }
 })();
+
+/**
+ * Checks if download button exists
+ * @param page
+ * @returns {Promise<any>}
+ */
+const validateEndpoint = page =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const exists = await page.evaluate(() => {
+        return !!document.querySelector("input[class=\"form-button\"]");
+      });
+
+      return resolve(exists);
+    } catch (e) {
+      return reject(e);
+    }
+
+  });
 
 /**
  * Generates random link endpoint
